34992eaaea963a53c64a1972230653100f7cd5fb
Daniel Magro <dando385@yahoo.com>
2024-04-25 20:55:41 -0400

    issues with bronwie compile

--- contracts/BondFactory.sol
+++ contracts/BondFactory.sol
@@ -4,7 +4,8 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts/access/AccessManaged.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
 import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
 contract BondFactory is ERC721, ERC721URIStorage, ERC721Burnable, AccessManaged, VRFConsumerBase {
